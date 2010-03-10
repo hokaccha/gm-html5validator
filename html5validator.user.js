7055f7ff7faaf48f785cde1258d25572291bbe35
hokaccha <k.hokamura@gmail.com>
2010-03-10 14:10:21 +0900

    onloadのhandlerを分離

--- html5validator.user.js
+++ html5validator.user.js
@@ -9,35 +9,42 @@
 // Version: 0.0.1
 
 var validator_url = 'http://html5.validator.nu/';
+
 var execute_validate = function() {
     GM_xmlhttpRequest({
         method: 'GET',
         url:    location.href,
-        onload: function(res) {
-            var boundary = get_boundary();
-            GM_xmlhttpRequest({
-                method: 'POST',
-                url: validator_url,
-                data: get_post_data(boundary, {
-                    out: 'json',
-                    content: res.responseText,
-                }),
-                headers: {
-                    'Content-type': 'multipart/form-data; boundary=' + boundary,
-                },
-                onload: function(r) {
-                    var data = eval('(' + r.responseText + ')');
-                    if (data.messages.length) {
-                        console.log('invalid');
-                        console.log(data.messages);
-                    }
-                    else {
-                        console.log('valid');
-                    }
-                }
-            });
-        },
+        onload: on_load_self,
     });
+};
+
+var on_load_self = function(res) {
+    var boundary = get_boundary();
+    var post_data = get_post_data(boundary, {
+        out: 'json',
+        content: res.responseText,
+    });
+    var headers =  {
+        'Content-type': 'multipart/form-data; boundary=' + boundary,
+    };
+    GM_xmlhttpRequest({
+        method:  'POST',
+        url:     validator_url,
+        data:    post_data,
+        headers: headers,
+        onload:  on_load_validate,
+    });
+};
+
+var on_load_validate = function(res) {
+    var data = eval( '(' + res.responseText + ')' );
+    if (data.messages.length) {
+        console.log('invalid');
+        console.log(data.messages);
+    }
+    else {
+        console.log('valid');
+    }
 };
 
 // get random boundary string
