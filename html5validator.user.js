dd1ef5d047b2fce70ce38ee2d7a34a530f19fe4a
hokaccha <k.hokamura@gmail.com>
2010-03-10 11:10:01 +0900

    リファクタ

--- html5validator.user.js
+++ html5validator.user.js
@@ -7,34 +7,62 @@
 //
 // Auther:  Kazuhito Hokamura
 // Version: 0.0.1
-//
 
-GM_xmlhttpRequest({
-    method: 'GET',
-    url:    location.href,
-    onload: function(res) {
-        var lb = "\r\n";
-        var boundary = '---------------------------separator';
-        var ct = function(c) { return 'Content-Disposition: form-data; name="'+c+'"' };
-        var data = '--' + boundary + lb + ct('out') + lb + lb + 'json' + lb + '--' + boundary + lb + ct('content') + lb + lb + res.responseText + lb + '--' + boundary + '--';
-        var url = 'http://html5.validator.nu/';
-        GM_xmlhttpRequest({
-            method: 'POST',
-            url: url,
-            data: data,
-            headers: {
-                'Content-type': 'multipart/form-data; boundary=' + boundary,
-            },
-            onload: function(r) {
-                var data = eval('(' + r.responseText + ')');
-                if (data.messages.length) {
-                    console.log('invalid');
-                    console.log(data.messages);
+var validator_url = 'http://html5.validator.nu/';
+var boundary = (function() {
+    var n = 50;
+    var c = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    c = c.split('');
+    var s = '';
+    for (var i = 0; i < n; i++) {
+        s += a[Math.floor(Math.random() * a.length)];
+    }
+    return s;
+})();
+
+var get_post_data = function(html) {
+    var line_break = "\r\n";
+    var get_content = function(key, val) {
+        return [
+            'Content-Disposition: form-data; name="' + key + '"',
+            '',
+            val
+        ].join(line_break);
+    };
+    var data = [
+        '--' + boundary,
+        get_content('out', 'json'),
+        '--' + boundary,
+        get_content('content', html),
+        '--' + boundary + '--'
+    ].join(line_break);
+};
+
+var execute_validate = function() {
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url:    location.href,
+        onload: function(res) {
+            GM_xmlhttpRequest({
+                method: 'POST',
+                url: validator_url,
+                data: get_post_data(res.responseText),
+                headers: {
+                    'Content-type': 'multipart/form-data; boundary=' + boundary,
+                },
+                onload: function(r) {
+                    var data = eval('(' + r.responseText + ')');
+                    if (data.messages.length) {
+                        console.log('invalid');
+                        console.log(data.messages);
+                    }
+                    else {
+                        console.log('valid');
+                    }
                 }
-                else {
-                    console.log('valid');
-                }
-            }
-        });
-    },
-});
+            });
+        },
+    });
+};
+
+execute_validate();
